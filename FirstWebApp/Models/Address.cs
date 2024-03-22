77ba640d0728ff789a69e2fc8db30260817c83e4
nagavardhini29 <ngarugu@asu.edu>
2024-03-22 16:31:30 -0500

    initial commit
    initial commit

---
+++ FirstWebApp/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstWebApp.Models
+{
+	public class Address
+	{
+        [Key]
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
+
