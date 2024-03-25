69a1da0d2b1773c3c17417547a8147478091b5e0
nagavardhini29 <ngarugu@asu.edu>
2024-03-25 18:14:26 -0500

    creating AppUser, Club models and ClubCategory enums
    creating AppUser, Club models and ClubCategory enums

---
+++ FirstWebApp/Data/Enum/ClubCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FirstWebApp.Data.Enum
+{
+	public enum ClubCategory
+	{
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance
+    }
+}
+
---
+++ FirstWebApp/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FirstWebApp.Models
+{
+	public class AppUser
+	{
+		public int? Pace { get; set; }
+		public int Mileage { get; set; }
+		public Address? Address { get; set; }
+	}
+}
+
