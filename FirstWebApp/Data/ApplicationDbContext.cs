6eac6e3a42be42ee3444bf96391f96215fe28f94
vardhinig10 <vardhinig10@gmail.com>
2024-03-27 09:26:58 -0500

    EF Core
    EF Core

---
+++ FirstWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FirstWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstWebApp.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
+
--- FirstWebApp/Models/AppUser.cs
+++ FirstWebApp/Models/AppUser.cs
@@ -6,6 +6,8 @@ namespace FirstWebApp.Models
 		public int? Pace { get; set; }
 		public int Mileage { get; set; }
 		public Address? Address { get; set; }
+		public ICollection<Club> Clubs { get; set; }
+		public ICollection<Race> Races { get; set; }
 	}
 }
 
