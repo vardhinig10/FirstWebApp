7ccd387711d34d1513dab9a213f5bd5187a3b2a9
vardhinig10 <vardhinig10@gmail.com>
2024-04-01 18:47:02 -0500

    initial club and home page view setup
    initial club and home page view setup

--- FirstWebApp/Controllers/RaceController.cs
+++ FirstWebApp/Controllers/RaceController.cs
@@ -2,26 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using FirstWebApp.Data;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace FirstWebApp.Controllers
 {
-    public class ClubController : Controller
+    public class RaceController : Controller
     {
-        private readonly ApplicationDbContext _context;
-
-        public ClubController(ApplicationDbContext context) 
-        {
-            _context = context;
-        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<Models.Club> clubs = _context.Clubs.ToList();
-            return View(clubs);
+            return View();
         }
     }
 }
