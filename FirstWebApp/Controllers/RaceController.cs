3e0d2618f31fffa0ea075837fc63b6c1e31b86b3
vardhinig10 <vardhinig10@gmail.com>
2024-04-02 16:07:29 -0500

    club and races pages initial view setup
    club and races pages initial view setup

--- FirstWebApp/Controllers/RaceController.cs
+++ FirstWebApp/Controllers/RaceController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FirstWebApp.Data;
+using FirstWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +12,17 @@ namespace FirstWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
