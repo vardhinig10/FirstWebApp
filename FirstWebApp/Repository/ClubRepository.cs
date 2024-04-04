25a59677364e186a36d8969a92e0969ef253509e
vardhinig10 <vardhinig10@gmail.com>
2024-04-04 17:47:38 -0500

    ClubRepository
    ClubRepository

---
+++ FirstWebApp/Repository/ClubRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using FirstWebApp.Data;
+using FirstWebApp.Interfaces;
+using FirstWebApp.Models;
+
+namespace FirstWebApp.Repository
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Club club)
+        {
+            _context.Add(club);
+            return Save();
+        }
+
+        public bool Delete(Club club)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Club>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Club> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Club>> GetClubByCity(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Club club)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
